07b2d68a1d55930efc476fb0e426cea1bd06fddf
junbeom <jach1206@naver.com>
2025-07-20 19:19:39 +0900

    fix: 예외 핸들러 root config 등록

--- src/main/java/org/scoula/global/common/config/RootConfig.java
+++ src/main/java/org/scoula/global/common/config/RootConfig.java
@@ -1,9 +1,7 @@
 package org.scoula.global.common.config;
 
-import java.util.Map;
+import javax.sql.DataSource;
 
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.flywaydb.core.Flyway;
 import org.mybatis.spring.SqlSessionFactoryBean;
@@ -17,76 +15,80 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 
-import javax.sql.DataSource;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 
 @Configuration
 @PropertySource({"classpath:/application.properties"})
 @MapperScan(basePackages = {"org.scoula.domain.member.mapper"}) // 게시판과 회원 도메인 매퍼 스캔
 @ComponentScan(basePackages = {
-    // "org.scoula.service",
-    "org.scoula.domain.member.service",  // 도메인 객체를 포함하기 위해 추가
-    "org.scoula.global.swagger.config"  // Swagger 설정을 포함하기 위해 추가
+	// "org.scoula.service",
+	"org.scoula.domain.member.service",  // 도메인 객체를 포함하기 위해 추가
+	"org.scoula.global.swagger.config",  // Swagger 설정을 포함하기 위해 추가
+	"org.scoula.global.kafka", // kafka 설정 포함
+	"org.scoula.global.exception" // exception handler 등록
 })
 public class RootConfig {
-    @Value("${jdbc.driver}") String driver;
-    @Value("${jdbc.url}") String url;
-    @Value("${jdbc.username}") String username;
-    @Value("${jdbc.password}") String password;
+	@Value("${jdbc.driver}")
+	String driver;
+	@Value("${jdbc.url}")
+	String url;
+	@Value("${jdbc.username}")
+	String username;
+	@Value("${jdbc.password}")
+	String password;
 
-    @Bean
-    public DataSource dataSource() {
-        HikariConfig config = new HikariConfig();
-        config.setDriverClassName(driver);
-        config.setJdbcUrl(url);
-        config.setUsername(username);
-        config.setPassword(password);
-        config.setMaximumPoolSize(50);
-        config.setMinimumIdle(5);
-        config.setConnectionTimeout(30000);
-        config.setIdleTimeout(600000);
-        config.setMaxLifetime(1800000);
-        HikariDataSource dataSource = new HikariDataSource(config);
-        return dataSource;
-    }
+	@Bean
+	public DataSource dataSource() {
+		HikariConfig config = new HikariConfig();
+		config.setDriverClassName(driver);
+		config.setJdbcUrl(url);
+		config.setUsername(username);
+		config.setPassword(password);
+		config.setMaximumPoolSize(50);
+		config.setMinimumIdle(5);
+		config.setConnectionTimeout(30000);
+		config.setIdleTimeout(600000);
+		config.setMaxLifetime(1800000);
+		HikariDataSource dataSource = new HikariDataSource(config);
+		return dataSource;
+	}
 
+	@Autowired
+	ApplicationContext applicationContext;
 
-    @Autowired
-    ApplicationContext applicationContext;
+	@Bean
+	public Flyway flyway(DataSource dataSource) {
+		Flyway flyway = Flyway.configure()
+			.dataSource(dataSource)
+			.locations("classpath:db/migration")
+			.baselineVersion("0.0")  // 초기 마이그레이션 버전 설정
+			.baselineOnMigrate(true)
+			.encoding("UTF-8")
+			.cleanDisabled(true)  // 안전을 위해 clean 비활성화
+			.validateMigrationNaming(true)
+			.load();
+		// Flyway 설정
 
-    @Bean
-    public Flyway flyway(DataSource dataSource) {
-        Flyway flyway = Flyway.configure()
-            .dataSource(dataSource)
-            .locations("classpath:db/migration")
-            .baselineVersion("0.0")  // 초기 마이그레이션 버전 설정
-            .baselineOnMigrate(true)
-            .encoding("UTF-8")
-            .cleanDisabled(true)  // 안전을 위해 clean 비활성화
-            .validateMigrationNaming(true)
-            .load();
-        // Flyway 설정
+		// 수동으로 migrate 호출
+		flyway.migrate();
 
+		return flyway;
+	}
 
-        // 수동으로 migrate 호출
-        flyway.migrate();
+	@Bean
+	public SqlSessionFactory sqlSessionFactory() throws Exception {
+		SqlSessionFactoryBean sqlSessionFactory = new SqlSessionFactoryBean();
+		sqlSessionFactory.setConfigLocation(
+			applicationContext.getResource("classpath:/mybatis-config.xml")
+		);
+		sqlSessionFactory.setDataSource(dataSource());
+		return (SqlSessionFactory)sqlSessionFactory.getObject();
+	}
 
-        return flyway;
-    }
-
-    @Bean
-    public SqlSessionFactory sqlSessionFactory() throws Exception {
-        SqlSessionFactoryBean sqlSessionFactory = new SqlSessionFactoryBean();
-        sqlSessionFactory.setConfigLocation(
-            applicationContext.getResource("classpath:/mybatis-config.xml")
-        );
-        sqlSessionFactory.setDataSource(dataSource());
-        return (SqlSessionFactory) sqlSessionFactory.getObject();
-    }
-
-    @Bean
-    public DataSourceTransactionManager transactionManager() {
-        return new DataSourceTransactionManager(dataSource());
-    }
-
+	@Bean
+	public DataSourceTransactionManager transactionManager() {
+		return new DataSourceTransactionManager(dataSource());
+	}
 
 }
