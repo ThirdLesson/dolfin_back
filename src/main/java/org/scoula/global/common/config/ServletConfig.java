cf16b1dfcdf65acd2ac537ff4b243c66d8aa5fdf
khozan0120 <fumajin0120@gmail.com>
2025-07-24 01:20:11 +0900

    [Refactor] rootconfig, ServletConfig 매퍼스캔,컴포넌트스캔,컨트롤러에다가 금융회사 관련 스캔 추가

--- src/main/java/org/scoula/global/common/config/ServletConfig.java
+++ src/main/java/org/scoula/global/common/config/ServletConfig.java
@@ -11,7 +11,8 @@ import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan(basePackages = {"org.scoula.domain.controller"})
+@ComponentScan(basePackages = {"org.scoula.domain.controller",
+"org.scoula.domain"})
 public class ServletConfig implements WebMvcConfigurer {
 
     @Override
@@ -19,19 +20,19 @@ public class ServletConfig implements WebMvcConfigurer {
         registry
                 .addResourceHandler("/resources/**") // url이 /resources/로 시작하는 모든 경로
                 .addResourceLocations("/resources/"); // webapp/resources/경로로 매핑
-        
+
         // Swagger UI 리소스를 위한 핸들러 설정
         registry.addResourceHandler("/swagger-ui.html")
                 .addResourceLocations("classpath:/META-INF/resources/");
-        
+
         // Swagger WebJar 리소스 설정
         registry.addResourceHandler("/webjars/**")
                 .addResourceLocations("classpath:/META-INF/resources/webjars/");
-        
+
         // Swagger 리소스 설정
         registry.addResourceHandler("/swagger-resources/**")
                 .addResourceLocations("classpath:/META-INF/resources/");
-        
+
         registry.addResourceHandler("/v2/api-docs")
                 .addResourceLocations("classpath:/META-INF/resources/");
     }
