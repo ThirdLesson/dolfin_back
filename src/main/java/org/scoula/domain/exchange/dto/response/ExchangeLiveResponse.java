6eb4189125e01a1c67d0663dfbf5be98683fa4a8
bagjuna <juna.park124@gmail.com>
2025-08-04 17:03:29 +0900

    [Feat]: exchange에 실시간 환율 api (#86)
    - 프론트에서 5분마다 환율 요청 보내면
    - 5분마다 실시간 환율 받는 api 개발

---
+++ src/main/java/org/scoula/domain/exchange/dto/response/ExchangeLiveResponse.java
@@ -0,0 +1,10 @@
+package org.scoula.domain.exchange.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class ExchangeLiveResponse {
+	public String text;
+}
