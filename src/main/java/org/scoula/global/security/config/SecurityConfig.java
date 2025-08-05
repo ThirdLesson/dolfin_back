bb1ddda7e53a7bcf01fbb0751098c6b121189523
bagjuna <juna.park124@gmail.com>
2025-08-05 10:10:51 +0900

    [HotFix]: cors 추가

--- src/main/java/org/scoula/global/security/config/SecurityConfig.java
+++ src/main/java/org/scoula/global/security/config/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 
 		corsConfiguration.setAllowedOriginPatterns(
-			List.of("http://localhost:5173", "https://localhost:5173"));
+			List.of("http://localhost:5173", "https://localhost:5173","http://dolfin-digital.vercel.app","https://dolfin-digital.vercel.app"));
 
 		corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS", "PATCH"));
 		corsConfiguration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
