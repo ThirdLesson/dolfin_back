05bac91a6fb05f27b8919151f086801bfc2b0512
junbeom <jach1206@naver.com>
2025-08-06 14:45:11 +0900

    feat: 사용자에게 하루 전 알림을 보내는 기능 개발

---
+++ src/main/java/org/scoula/global/firebase/event/RemittanceGroupChargeNoticeEvent.java
@@ -0,0 +1,14 @@
+package org.scoula.global.firebase.event;
+
+import java.util.List;
+
+import org.scoula.domain.remittancegroup.batch.dto.MemberWithInformationDto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class RemittanceGroupChargeNoticeEvent {
+	private final List<MemberWithInformationDto> members;
+}
