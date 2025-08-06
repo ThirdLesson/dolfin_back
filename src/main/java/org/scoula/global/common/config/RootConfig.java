6e205ab54c5e7f55ab9b61263617b9c9d1e9ab64
junbeom <jach1206@naver.com>
2025-08-06 11:33:57 +0900

    feat: 스프링 배치 및 레디스 분산락 의존성 추가

--- src/main/java/org/scoula/global/common/config/RootConfig.java
+++ src/main/java/org/scoula/global/common/config/RootConfig.java
@@ -6,14 +6,18 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.flywaydb.core.Flyway;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
+import org.scoula.domain.remittancegroup.batch.reader.RemittanceGroupReader;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
@@ -31,64 +35,69 @@ import com.zaxxer.hikari.HikariDataSource;
  */
 @Configuration
 @PropertySource({"classpath:/application.properties"})
+@EnableBatchProcessing
+@EnableScheduling
 @MapperScan(basePackages = {"org.scoula.domain.**.mapper"}) // 게시판과 회원 도메인 매퍼 스캔
 @ComponentScan(basePackages = {
 	// "org.scoula.service",
 	"org.scoula.domain.**.service",  // 도메인 객체를 포함하기 위해 추가
+	"org.scoula.domain.remittancegroup.batch", // spring batch 설정 추가
 	"org.scoula.global.swagger.config",  // Swagger 설정을 포함하기 위해 추가
 	"org.scoula.global.kafka", // kafka 설정 포함
 	"org.scoula.global.exception", // exception handler 등록
 	"org.scoula.global.redis", // 공통 설정 포함
 	"org.scoula.global.security", // security 설정 포함
 	"org.scoula.global.firebase", // firebase 설정 포함
+	"org.scoula.global.batch.config",
 })
+@Import({RemittanceGroupReader.class})
 public class RootConfig {
-    @Value("${jdbc.driver}")
-    String driver;
-    @Value("${jdbc.url}")
-    String url;
-    @Value("${jdbc.username}")
-    String username;
-    @Value("${jdbc.password}")
-    String password;
+	@Value("${jdbc.driver}")
+	String driver;
+	@Value("${jdbc.url}")
+	String url;
+	@Value("${jdbc.username}")
+	String username;
+	@Value("${jdbc.password}")
+	String password;
 
-    @Bean
-    public DataSource dataSource() {
-        HikariConfig config = new HikariConfig();
-        config.setDriverClassName(driver);
-        config.setJdbcUrl(url);
-        config.setUsername(username);
-        config.setPassword(password);
-        config.setMaximumPoolSize(50);
-        config.setMinimumIdle(5);
-        config.setConnectionTimeout(30000);
-        config.setIdleTimeout(600000);
-        config.setMaxLifetime(1800000);
-        HikariDataSource dataSource = new HikariDataSource(config);
-        return dataSource;
-    }
+	@Bean
+	public DataSource dataSource() {
+		HikariConfig config = new HikariConfig();
+		config.setDriverClassName(driver);
+		config.setJdbcUrl(url);
+		config.setUsername(username);
+		config.setPassword(password);
+		config.setMaximumPoolSize(50);
+		config.setMinimumIdle(5);
+		config.setConnectionTimeout(30000);
+		config.setIdleTimeout(600000);
+		config.setMaxLifetime(1800000);
+		HikariDataSource dataSource = new HikariDataSource(config);
+		return dataSource;
+	}
 
-    @Autowired
-    ApplicationContext applicationContext;
+	@Autowired
+	ApplicationContext applicationContext;
 
-    @Bean
-    public Flyway flyway(DataSource dataSource) {
-        Flyway flyway = Flyway.configure()
-                .dataSource(dataSource)
-                .locations("classpath:db/migration")
-                .baselineVersion("0.0")  // 초기 마이그레이션 버전 설정
-                .baselineOnMigrate(true)
-                .encoding("UTF-8")
-                .cleanDisabled(true)  // 안전을 위해 clean 비활성화
-                .validateMigrationNaming(true)
-                .load();
-        // Flyway 설정
+	@Bean
+	public Flyway flyway(DataSource dataSource) {
+		Flyway flyway = Flyway.configure()
+			.dataSource(dataSource)
+			.locations("classpath:db/migration")
+			.baselineVersion("0.0")  // 초기 마이그레이션 버전 설정
+			.baselineOnMigrate(true)
+			.encoding("UTF-8")
+			.cleanDisabled(true)  // 안전을 위해 clean 비활성화
+			.validateMigrationNaming(true)
+			.load();
+		// Flyway 설정
 
-        // 수동으로 migrate 호출
-        flyway.migrate();
+		// 수동으로 migrate 호출
+		flyway.migrate();
 
-        return flyway;
-    }
+		return flyway;
+	}
 
 	@Bean
 	public SqlSessionFactory sqlSessionFactory() throws Exception {
@@ -100,10 +109,10 @@ public class RootConfig {
 		return sqlSessionFactory.getObject();
 	}
 
-    @Bean
-    public DataSourceTransactionManager transactionManager() {
-        return new DataSourceTransactionManager(dataSource());
-    }
+	@Bean
+	public DataSourceTransactionManager transactionManager() {
+		return new DataSourceTransactionManager(dataSource());
+	}
 
 	@Bean
 	public PasswordEncoder passwordEncoder() {
---
+++ src/main/java/org/scoula/global/redis/config/RedisShedLockConfig.java
@@ -0,0 +1,20 @@
+package org.scoula.global.redis.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import net.javacrumbs.shedlock.core.LockProvider;
+import net.javacrumbs.shedlock.provider.redis.spring.RedisLockProvider;
+import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
+
+@Configuration
+@EnableScheduling
+@EnableSchedulerLock(defaultLockAtMostFor = "PT10M") // 락 유지 시간 (10분)
+public class RedisShedLockConfig {
+	@Bean
+	public LockProvider lockProvider(RedisConnectionFactory redisConnectionFactory) {
+		return new RedisLockProvider(redisConnectionFactory);
+	}
+}
