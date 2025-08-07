9e448997ac5df4528e585c7b1fede63374f39109
junbeom <jach1206@naver.com>
2025-08-07 16:47:30 +0900

    feat: 어제의 회계 기록을 모두 가져오는 기능 개발

--- src/main/java/org/scoula/domain/ledger/mapper/LedgerVoucherMapper.java
+++ src/main/java/org/scoula/domain/ledger/mapper/LedgerVoucherMapper.java
@@ -1,9 +1,29 @@
 package org.scoula.domain.ledger.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.scoula.domain.ledger.batch.dto.LedgerVoucherWithEntryDTO;
 import org.scoula.domain.ledger.entity.LedgerVoucher;
 
 @Mapper
 public interface LedgerVoucherMapper {
 	void insert(LedgerVoucher ledgerVoucher);
+
+	@Select("""
+			SELECT 
+				lv.ledger_voucher_id,
+				lv.voucher_no,
+				lv.entry_date,
+				lv.type,
+				le.ledger_entry_id,
+				le.amount,
+				le.ledger_type,
+				le.account_code_id
+			FROM ledger_voucher lv
+			JOIN ledger_entry le ON lv.ledger_voucher_id = le.ledger_voucher_id
+			WHERE DATE(lv.entry_date) = CURDATE() - INTERVAL 1 DAY
+		""")
+	List<LedgerVoucherWithEntryDTO> findAllFromYesterday();
 }
