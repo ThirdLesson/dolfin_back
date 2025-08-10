8202799521d544f85bfe24ff7e811c6c5bdf0b47
khozan0120 <fumajin0120@gmail.com>
2025-08-10 20:39:09 +0900

    chore : 변수명 변경 MIN_RATE -> BASE_RATE

--- src/main/java/org/scoula/domain/financialproduct/constants/JeonseLoanRateType.java
+++ src/main/java/org/scoula/domain/financialproduct/constants/JeonseLoanRateType.java
@@ -1,13 +1,21 @@
 package org.scoula.domain.financialproduct.constants;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
+@ApiModel(description = "전세대출 금리 정렬 타입")
 public enum JeonseLoanRateType {
-	MIN_RATE("최저 금리"),
+	@ApiModelProperty("최소 금리")
+	BASE_RATE("최저 금리"),
+
+	@ApiModelProperty("최대 금리")
 	MAX_RATE("최고 금리"),
+
+	@ApiModelProperty("평균 금리")
 	AVG_RATE("평균 금리");
 
 	private final String name;
