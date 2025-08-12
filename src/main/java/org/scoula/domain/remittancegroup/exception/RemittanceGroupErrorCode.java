e2f93d2abdda83c43faa688e1662a91b8d5e15d9
bagjuna <juna.park124@gmail.com>
2025-08-12 13:15:17 +0900

    [Feat]: 단체 송금 최소, 여부 가입 api 개발 (#115)
    * Feat: 단체 송금 최소, 여부 가입 api 개발
    - 단체 송금 취소를 /check/info로 개발
    - 딘체 송금 여부 가입 /check/info로 개발
    
    * Feat: 단체 송금 최소, 여부 가입 api 개발
    - 단체 송금 취소를 /check/info로 개발
    - 딘체 송금 여부 가입 /check/info로 개발

--- src/main/java/org/scoula/domain/remittancegroup/exception/RemittanceGroupErrorCode.java
+++ src/main/java/org/scoula/domain/remittancegroup/exception/RemittanceGroupErrorCode.java
@@ -11,7 +11,9 @@ import lombok.Getter;
 public enum RemittanceGroupErrorCode implements ErrorCode {
 
 	ALREADY_JOINED_GROUP(HttpStatus.BAD_REQUEST, "RG-001", "이미 다른 단체 송금 그룹에 가입되어있는 회원입니다."),
-
+	NOT_EXIST_GROUP(HttpStatus.NOT_FOUND, "RG-002", "존재하지 않는 단체 송금 그룹입니다."),
+	CAN_NOT_GROUP_NUMBER_ZERO(HttpStatus.BAD_REQUEST, "RG-003", "단체 송금 그룹의 인원 수는 0명이 될 수 없습니다."),
+	MEMBER_COUNT_NUMBER_ZERO(HttpStatus.BAD_REQUEST, "RG-004", "단체 송금 그룹의 인원 수는 0명이 될 수 없습니다."),
 	;
 
 	private final HttpStatus httpStatus;
