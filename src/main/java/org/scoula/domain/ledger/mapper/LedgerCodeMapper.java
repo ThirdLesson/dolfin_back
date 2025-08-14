bba190d6eebfec60048d9f40671853e6f982068f
junbeom <jach1206@naver.com>
2025-08-14 09:49:04 +0900

    feat: 회계 코드 리스트 조회로 변경

--- src/main/java/org/scoula/domain/ledger/mapper/LedgerCodeMapper.java
+++ src/main/java/org/scoula/domain/ledger/mapper/LedgerCodeMapper.java
@@ -1,11 +1,18 @@
 package org.scoula.domain.ledger.mapper;
 
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
+import org.apache.ibatis.annotations.MapKey;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.scoula.domain.ledger.entity.LedgerCode;
 
 @Mapper
 public interface LedgerCodeMapper {
 	Optional<LedgerCode> findByName(String name);
+
+	@MapKey("name")
+	Map<String, LedgerCode> findByNames(@Param("names") List<String> names);
 }
