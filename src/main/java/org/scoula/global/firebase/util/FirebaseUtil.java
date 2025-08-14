fe2800a11436ae5f429bce55a87f8ec746d7e975
junbeom <jach1206@naver.com>
2025-08-14 10:29:52 +0900

    feat: fcm 알림 실패 추적을 위한 로그 수정

--- src/main/java/org/scoula/global/firebase/util/FirebaseUtil.java
+++ src/main/java/org/scoula/global/firebase/util/FirebaseUtil.java
@@ -2,6 +2,10 @@ package org.scoula.global.firebase.util;
 
 import java.util.List;
 
+import org.scoula.global.kafka.dto.Common;
+import org.scoula.global.kafka.dto.LogLevel;
+import org.scoula.global.kafka.producer.KafkaProducer;
+import org.scoula.global.kafka.util.LogMessageMapper;
 import org.springframework.stereotype.Component;
 
 import com.google.firebase.messaging.FirebaseMessaging;
@@ -9,8 +13,14 @@ import com.google.firebase.messaging.Message;
 import com.google.firebase.messaging.WebpushConfig;
 import com.google.firebase.messaging.WebpushNotification;
 
+import lombok.RequiredArgsConstructor;
+
 @Component
+@RequiredArgsConstructor
 public class FirebaseUtil {
+
+	private final KafkaProducer kafkaProducer;
+
 	public void sendNotices(List<String> tokens, String title, String body) {
 		for (String token : tokens) {
 			Message message = Message.builder()
@@ -24,7 +34,9 @@ public class FirebaseUtil {
 			try {
 				String send = FirebaseMessaging.getInstance().send(message);
 			} catch (Exception e) {
-				throw new RuntimeException(e);
+				kafkaProducer.sendToLogTopic(
+					LogMessageMapper.buildLogMessage(LogLevel.INFO, null, "fcm 알림 전송 실패", Common.builder().build(),
+						e.toString()));
 			}
 		}
 	}
@@ -41,7 +53,9 @@ public class FirebaseUtil {
 		try {
 			String send = FirebaseMessaging.getInstance().send(message);
 		} catch (Exception e) {
-			throw new RuntimeException(e);
+			kafkaProducer.sendToLogTopic(
+				LogMessageMapper.buildLogMessage(LogLevel.INFO, null, "fcm 알림 전송 실패", Common.builder().build(),
+					e.toString()));
 		}
 	}
 }
