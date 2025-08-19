3d335ad9060036273a625f2f8ee64ae4b512b504
호진 <fumajin0120@gmail.com>
2025-08-19 10:13:26 +0900

    chore: 코드 주석 제거
    불필요한 주석을 삭제하여 코드 가독성을 개선했습니다.

--- src/main/java/org/scoula/domain/financialproduct/depositsaving/service/DepositService.java
+++ src/main/java/org/scoula/domain/financialproduct/depositsaving/service/DepositService.java
@@ -12,19 +12,14 @@ import org.springframework.data.domain.Pageable;
 
 public interface DepositService {
 
-	// 예금 상품 정보만 저장
 	List<Deposit> fetchAndSaveDeposits();
 
-	// 우대조건 정보만 저장 - 기존에 저장된 예금 상품들과 연결
 	void fetchAndSaveSpclConditions(List<Deposit> savedDeposits);
 
-	// 예금 상품 리스트 조회(필터링 + 페이징)
 	Page<DepositsResponse> getDeposits(ProductPeriod productPeriod, List<DepositSpclConditionType> spclConditions,
 		Pageable pageable, Member member);
 
-	// 	에금 상품 상세 정보 조회
 	DepositsResponse getDepositDetail(Long depositId);
 
-	// 전체 조회용 (체류기간 필터링 없음)
 	Page<DepositsResponse> getAllDeposits(Pageable pageable);
 }
