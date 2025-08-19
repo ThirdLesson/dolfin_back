2b0a306f3dad8fde804463d3ddc0b62c33f13e2f
호진 <fumajin0120@gmail.com>
2025-08-19 11:28:21 +0900

    chore: 코드 주석 제거
    불필요한 주석을 삭제하여 코드 가독성을 개선했습니다.

--- src/main/java/org/scoula/global/common/config/ServletConfig.java
+++ src/main/java/org/scoula/global/common/config/ServletConfig.java
@@ -17,25 +17,21 @@ import org.springframework.web.servlet.view.JstlView;
 @EnableWebMvc
 @ComponentScan(basePackages = {
 	"org.scoula.domain.**.controller",
-	"org.scoula.global.exception" // exception handler 등록
+	"org.scoula.global.exception" 
 })
 public class ServletConfig implements WebMvcConfigurer {
 
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry
-			.addResourceHandler("/resources/**") // url이 /resources/로 시작하는 모든 경로
-			.addResourceLocations("/resources/"); // webapp/resources/경로로 매핑
-
-		// Swagger UI 리소스를 위한 핸들러 설정
+			.addResourceHandler("/resources/**") 
+			.addResourceLocations("/resources/"); 
 		registry.addResourceHandler("/swagger-ui.html")
 			.addResourceLocations("classpath:/META-INF/resources/");
 
-		// Swagger WebJar 리소스 설정
 		registry.addResourceHandler("/webjars/**")
 			.addResourceLocations("classpath:/META-INF/resources/webjars/");
 
-		// Swagger 리소스 설정
 		registry.addResourceHandler("/swagger-resources/**")
 			.addResourceLocations("classpath:/META-INF/resources/");
 
@@ -43,7 +39,6 @@ public class ServletConfig implements WebMvcConfigurer {
 			.addResourceLocations("classpath:/META-INF/resources/");
 	}
 
-	// jsp view resolver 설정
 	@Override
 	public void configureViewResolvers(ViewResolverRegistry registry) {
 		InternalResourceViewResolver bean = new InternalResourceViewResolver();
