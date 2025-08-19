04596987515f6f717d42b9949c193c7f176f2821
호진 <fumajin0120@gmail.com>
2025-08-19 10:24:15 +0900

    chore: 코드 주석 제거
    불필요한 주석을 삭제하여 코드 가독성을 개선했습니다.

--- src/main/java/org/scoula/domain/financialproduct/financialcompany/service/FinancialCompanyService.java
+++ src/main/java/org/scoula/domain/financialproduct/financialcompany/service/FinancialCompanyService.java
@@ -7,12 +7,9 @@ import java.util.List;
 
 public interface FinancialCompanyService {
 
-	// 금융회사 리스트 전체 조회
 	List<FinancialCompanyResponseDTO> getAll();
 
-	// API 호출 및 저장
 	List<FinancialCompanyResponseDTO> fetchAndSaveFinancialCompanies();
 
-	// 금융회사 아이디로 리스트 저장.
 	FinancialCompany getById(Long financialCompanyId);
 }
