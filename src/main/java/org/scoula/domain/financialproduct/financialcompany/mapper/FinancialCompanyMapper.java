f44d34c8634ec56e16bddc2a2ad2e8e3caee32d1
호진 <fumajin0120@gmail.com>
2025-08-19 10:19:49 +0900

    chore: 코드 주석 제거
    불필요한 주석을 삭제하여 코드 가독성을 개선했습니다.

--- src/main/java/org/scoula/domain/financialproduct/financialcompany/mapper/FinancialCompanyMapper.java
+++ src/main/java/org/scoula/domain/financialproduct/financialcompany/mapper/FinancialCompanyMapper.java
@@ -10,15 +10,11 @@ import java.util.Map;
 @Mapper
 public interface FinancialCompanyMapper {
 
-	// 전체 조회
 	List<FinancialCompany> selectAllFinancialCompany();
 
-	// 금융회사 코드로 조회
 	FinancialCompany selectByCode(String code);
 
-	// 여러 금융회사 데이터르 한 번에 저장
 	int insertBatch(List<FinancialCompany> newCompanies);
 
-	// 금융회사 ID 조회
 	FinancialCompany selectById(Long financialCompanyId);
 }
