91a6bb19f6278709eebd95d68702cde2eee4421a
호진 <fumajin0120@gmail.com>
2025-08-19 10:15:19 +0900

    chore: 코드 주석 제거
    불필요한 주석을 삭제하여 코드 가독성을 개선했습니다.

--- src/main/java/org/scoula/domain/financialproduct/depositsaving/service/SavingService.java
+++ src/main/java/org/scoula/domain/financialproduct/depositsaving/service/SavingService.java
@@ -15,20 +15,15 @@ import org.springframework.data.domain.Pageable;
 
 public interface SavingService {
 
-	// 적금상품 API를 호출해서 가져와 저장
 	List<Saving> fetchAndSaveSavings();
 
-	// 우대조건 정보만 저장 - 기존에 저장된 적금 상품들과 연결
 	void fetchAndSaveSpclConditions(List<Saving> savedSavings);
 
-	// // 적금 상품 리스트 조회(필터링+페이징)
 	Page<SavingsResponse> getSavings(ProductPeriod productPeriod, List<SavingSpclConditionType> spclConditions,
 		Pageable pageable, Member member);
 
-	// 	적금 상품 상세 정보 조회
 	SavingsResponse getSavingDetail(Long savingId);
 
-	// 	전체 조회용(체류기간 필터링 없음)
 	Page<SavingsResponse> getAllSavings(Pageable pageable);
 }
 
