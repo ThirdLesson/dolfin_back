69589cdb07c7d90db5cf7cceb90a2bc8b51b5066
호진 <fumajin0120@gmail.com>
2025-08-19 11:14:45 +0900

    chore: 코드 주석 제거
    불필요한 주석을 삭제하여 코드 가독성을 개선했습니다.

--- src/main/java/org/scoula/domain/member/mapper/MemberMapper.java
+++ src/main/java/org/scoula/domain/member/mapper/MemberMapper.java
@@ -13,25 +13,18 @@ import org.scoula.domain.remittancegroup.batch.dto.MemberWithInformationDto;
 @Mapper
 public interface MemberMapper {
 
-	// 회원 전체 조회
 	List<Member> selectAllMembers();
 
-	// 회원 상세 조회
 	Member selectMemberById(@Param("memberId") Long memberId);
 
-	// 로그인 ID로 회원 조회
 	Member selectMemberByLoginId(@Param("loginId") String loginId);
 
-	// 회원 등록
 	int insertMember(Member member);
 
-	// 회원 정보 수정
 	int updateMember(Member member);
 
-	// 회원 삭제
 	int deleteMember(@Param("memberId") Long memberId);
 
-	// 로그인 ID 중복 체크
 	int checkLoginIdDuplicate(@Param("loginId") String loginId);
 
 	@Update("UPDATE member SET connected_id = #{connectedId} WHERE member_id = #{memberId}")
