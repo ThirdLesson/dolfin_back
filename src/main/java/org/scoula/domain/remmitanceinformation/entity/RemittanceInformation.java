3e55e6efadd4c64dd15d952cd796a33c5b1fa535
호진 <fumajin0120@gmail.com>
2025-08-19 11:21:53 +0900

    chore: 코드 주석 제거
    불필요한 주석을 삭제하여 코드 가독성을 개선했습니다.

--- src/main/java/org/scoula/domain/remmitanceinformation/entity/RemittanceInformation.java
+++ src/main/java/org/scoula/domain/remmitanceinformation/entity/RemittanceInformation.java
@@ -11,9 +11,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-/**
- * 사용자_단체 송금 정보
- */
+
 @Getter
 @Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -21,18 +19,18 @@ import lombok.Setter;
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 public class RemittanceInformation extends BaseEntity {
 
-	private Long remittanceInformationId;   // PK
-	private String receiverBank;          // 수취인 은행 이름
-	private String swiftCode;             // SWIFT 코드
-	private String routerCode;            // 라우터 코드(미국)
-	private String receiverAccount;       // 수취인 계좌
-	private String receiverName;          // 수취인 이름
-	private String receiverNationality;   // 수취인 국가
-	private String receiverAddress;       // 수취인 주소
-	private String purpose;               // 거래목적
-	private BigDecimal amount;            // 환전을 원하는 금액(원화)
-	private Integer transmitFailCount;    // 송금 실패 카운트 (2회 이상 시 그룹 탈퇴)
-	private IntermediaryBankCommission intermediaryBankCommission; // 중계 수수료 부담 방식
+	private Long remittanceInformationId;   
+	private String receiverBank;         
+	private String swiftCode;            
+	private String routerCode;          
+	private String receiverAccount;       
+	private String receiverName;         
+	private String receiverNationality;   
+	private String receiverAddress;      
+	private String purpose;              
+	private BigDecimal amount;          
+	private Integer transmitFailCount;    
+	private IntermediaryBankCommission intermediaryBankCommission; 
 
 	public void updateRouterCode(String routerCode) {
 		this.routerCode = routerCode;
